554c72d30fb497611748c1f9006baab1e05714cd
Becherer <SMB0312@daugherty.com>
2019-08-19 15:38:53 -0500

    Removed file.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import App from './components/App';
 
 render(<App />,document.getElementById('root'));
 
-//lets webpackHotMiddleware know to update this module
-// if (module.hot) {
-//   module.hot.accept();
-// }
 module.hot.accept('./components/App.js', () => {
      const NextRootContainer = require('./components/App.js').default;
      render(<NextRootContainer />, document.getElementById('root'));
